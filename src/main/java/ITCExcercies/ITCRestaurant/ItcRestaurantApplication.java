6f86c8e0addae7b9860935c87575313d44432664
Wagner Zoltan <zoltwagner@gmail.com>
2020-07-15 15:30:05 +0200

    First steps
    *Create the skeleton of the project
    *Adding the main dependencies
    *Adding H" database handling, because its portability

---
+++ src/main/java/ITCExcercies/ITCRestaurant/ItcRestaurantApplication.java
@@ -0,0 +1,13 @@
+package ITCExcercies.ITCRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItcRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItcRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ITCExcercies/ITCRestaurant/ItcRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package ITCExcercies.ITCRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItcRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
